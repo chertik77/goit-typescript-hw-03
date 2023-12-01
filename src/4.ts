81ad53d33a94ffda051c333aa2e711e1e113ac95
chertik77 <babychdenya@gmail.com>
2023-12-01 10:51:04 +0000

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random()
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
 
-house.comeIn(person);
+  getKey() {
+    return this.key
+  }
+}
 
+abstract class House {
+  protected door: boolean = false
+  protected tenants: Person[] = []
 
-export {};
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person)
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true
+    }
+  }
+}
+
+const key = new Key()
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+house.comeIn(person)
+
+export {}
